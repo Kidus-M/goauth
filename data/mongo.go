a9a9d73e33903b4633f71bbd8a211f79edc0b4fa
kidus-m <kidus0237@gmail.com>
2025-11-14 18:47:04 +0300

    data done

--- data/mongo.go
+++ data/mongo.go
@@ -11,13 +11,11 @@ import (
 // NewMongoClient creates and returns a connected *mongo.Client
 func NewMongoClient(ctx context.Context, uri string) (*mongo.Client, error) {
 	opts := options.Client().ApplyURI(uri)
-	// Set sensible defaults
 	opts.SetServerSelectionTimeout(10 * time.Second)
 	client, err := mongo.Connect(ctx, opts)
 	if err != nil {
 		return nil, err
 	}
-	// Ping to verify connection
 	if err := client.Ping(ctx, nil); err != nil {
 		_ = client.Disconnect(ctx)
 		return nil, err
